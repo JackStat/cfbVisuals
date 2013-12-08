3df467a7c1c64cc7484088504df6c7038838a935
Tyler Hunt <tyler.hunt@utah.edu>
2013-12-07 19:56:33 -0700

    Initializing...

---
+++ server.R
@@ -0,0 +1,51 @@
+
+require(shiny)
+require(rCharts)
+require(plyr)
+options(RCHART_WIDTH = 800, RCHART_HEIGHT = 432)
+
+ranks<-read.csv("Data/y2013w14Rank.csv")
+names(ranks) = gsub("\\.", "", names(ranks))
+ranks<-ranks[rowSums(cbind(ranks$Wins, ranks$Losses))>5,]
+
+ranks$Conference<-droplevels(ranks$Conference)
+
+ranks$Conference<-as.numeric(ranks$Conference)
+
+ranks<-arrange(ranks, Conference)
+
+shinyServer(function(input, output) {
+  formulaText <- reactive({
+  paste(input$x, "~", input$y)
+  })
+  
+  sizing <- reactive({
+    paste("#! function(d){return d.", input$size, "} !#", sep="")
+  })
+
+  output$myChart <- renderChart({
+  
+  n1 <- nPlot(as.formula(formulaText()), 
+              data = ranks,
+              group=input$grp,
+              type = 'scatterChart')
+  
+#n1$chart(size = '#! function(d){return d.Score} !#')
+  n1$chart(size = sizing())
+ 
+  n1$chart(tooltipContent= "#! function(key, x, y, e){
+  return '<b>Team:</b> ' + e.point.Team + '<br/>' +
+    '<b>Rank: </b>' + e.point.Rank + '<br/>' + 
+    '<b>Score: </b>' + e.point.Score + '<br/>' +  
+    '<b>Wins: </b>' + e.point.Wins + '<br/>' + 
+    '<b>Losses: </b>' + e.point.Losses
+} !#")
+  
+  #n1$set(width = 800, height = 600, legendPosition = "bottom")
+  
+  n1$addParams(dom = 'myChart')
+
+  return(n1)
+
+  })
+})
---
+++ ui.R
@@ -0,0 +1,57 @@
+require(rCharts)
+shinyUI(pageWithSidebar(
+headerPanel("Visualizing College Football With rCharts"),
+
+sidebarPanel(
+  
+selectInput(inputId = "x",
+            label = "Choose X",
+            choices = c('Rank', 'Score', 'Elo', 'TrueMargin', 'Wins', 'Losses'),
+            selected = "Elo"),
+
+selectInput(inputId = "y",
+            label = "Choose Y",
+            choices = c('Rank', 'Score', 'Elo', 'TrueMargin', 'Wins', 'Losses'),
+            selected = "TrueMargin"),
+
+selectInput(inputId = "grp",
+            label = "Choose Group",
+            choices = c('Conference', 'Wins', 'Losses'),
+            selected = "Conference"),
+
+selectInput(inputId = "size",
+            label = "Choose Size",
+            choices = c('Rank', 'Score', 'Elo', 'TrueMargin', 'Wins', 'Losses'),
+            selected = "Score"),
+
+helpText("Conference Key:",
+         br(),
+         "1 = American Athletic Conference",
+         br(),
+         "2 = Atlantic Coast Conference",
+         br(),
+         "3 = Big 12 Conference",
+         br(),
+         "4 = Big Ten Conference",
+         br(),
+         "5 = Conference USA",
+         br(),
+         "6 = Independent",
+         br(),
+         "7 = Mid-American Conference",
+         br(),
+         "8 = Mountain West Conference",
+         br(),
+         "9 = PAC-12 Conference",
+         br(),
+         "10 =Southeastern Conference",
+         br(),
+         "11 = Sun Belt Conference")
+
+),
+
+mainPanel(
+showOutput("myChart", "nvd3")
+  )
+))
+
